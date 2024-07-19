d1363ca068969df3e9cab249ad6659e8765badcd
shahbazali38 <159769905+shahbazali38@users.noreply.github.com>
2024-07-19 10:54:53 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,60 @@
+#!/usr/bin/env node
+
+async function main() {
+    const inquirer = await import("inquirer");
+    let user = await inquirer.default.prompt([
+        {
+            type: "string",
+            name: "cardholdername",
+            message: "Welcome Shahbaz Ali",
+        },
+        {
+            type: "number",
+            name: "pinCode",
+            message: "Enter your 4 digit pinCode",
+        },
+        {
+            type: "list",
+            name: "accountType",
+            message: "Select your account type",
+            choices: ["Current", "Saving"],
+        },
+        {
+            type: "list",
+            name: "transitionType",
+            message: "Select your transition type",
+            choices: ["cash", "withdrawal"],
+        },
+        {
+            type: "list",
+            name: "amount",
+            message: "Select your amount",
+            choices: [1000, 2000, 5000, 10000],
+            when(user) {
+                return user.transitionType === "cash";
+            }
+        },
+        {
+            type: "number",
+            name: "amount",
+            message: "Enter your amount",
+            when(user) {
+                return user.transitionType === "withdrawal";
+            }
+        }
+    ]);
+
+    // Final Slip
+    if (user.pinCode) {
+        const balance = Math.floor(Math.random() * 10000 + 1);
+        console.log(balance);
+        const enteramount = user.account;
+
+        if (enteramount <= balance) {
+            const remaining = balance - enteramount;
+            console.log(`you have withdrawal rupees ${enteramount} and you have remaining balance ${remaining}`);
+        }
+    }
+}
+
+main(); // Call the asynchronous function to start the program
